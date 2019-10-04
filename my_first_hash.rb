5657022529408a41a11f9f93892469761d2607b3
Jazmin M <jazminmerckel@gmail.com>
2019-10-04 15:21:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,11 +1,21 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+def my_hash {
+ 
+  "fruit" => "apple",
+  "vegetable" => "cucumber",
+  "candy" => "Reeses" 
   
+}
+
 end
 
 
-def shipping_manifest
+def shipping_manifest {
+ 
+  "apple" => "a delicious fruit",
+  "this readme" => "wonderful and informative piece of reading",
+  "Moby Dick" => "a novel by my good friend, Herman Melville"
+ 
+}
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
